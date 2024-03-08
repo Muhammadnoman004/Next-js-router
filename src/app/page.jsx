d6462b57fdf04c8bf686a0e62548611a32585cd2
Noman Shamim <nomanshamim2004@gmail.com>
2024-03-08 17:45:54 +0500

    Counter...

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 export default function HomePage() {
+  let [Count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(Count + 1)
+  }
+  const decrement = () => {
+    setCount(Count - 1)
+    if (Count == 0) {
+      setCount(Count = 0)
+    }
+
+  }
+
   return (
-    <div>Home Page...</div>
+    <div style={{ textAlign: "center" }}>
+      <h1 >...Counter...</h1>
+      <h1>{Count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decerment</button>
+    </div>
   )
 }
